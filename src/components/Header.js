1955e86b370f978aff0f3903db1b7ce211239873
Alex Ramos <alex@example.com>
2018-11-08 14:25:42 +0100

    Solucionado problema cone el <Header>

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-<<<<<<< HEAD
 const Header = ({ tagline }) => (
   <header className="top">
     <h1>
@@ -16,26 +15,5 @@ const Header = ({ tagline }) => (
     </h3>
   </header>
 );
-=======
-class Header extends React.Component {
-  render() {
-    return (
-      <header className="top">
-        <h1>
-          Catch
-          <span className="ofThe">
-            <span className="of">Of</span>
-            <span className="the">The</span>
-          </span>
-          Day
-        </h1>
-        <h3 className="tagline">
-          <span>Fresh Daily</span>
-        </h3>
-      </header>
-    );
-  }
-}
->>>>>>> e03168d159d8d705a739851f085d9852820075f8
 
 export default Header;

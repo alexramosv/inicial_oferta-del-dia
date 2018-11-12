8f09b2699ffae0e5e9e2efe9ad2cc47930fa7570
Alex Ramos <alex@example.com>
2018-11-12 14:54:14 +0100

    WIP: Final de clase añadido el holita vecinito

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,12 +1,17 @@
 import React from "react";
 
 class StorePicker extends React.Component {
+  handleClick = () => {
+    console.log(this);
+    alert("Hola, holita vecinitoooooo!!");
+  };
+
   render() {
     return (
       <form className="store-selector">
         <h2>Please Enter a Store</h2>
         <input type="text" required placeholder="Store Name" />
-        <button type="submit">
+        <button onClick={this.handleClick} type="submit">
           Visit Store{" "}
           <span role="img" aria-labelledby="tenedor">
             🍴

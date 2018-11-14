423ae48253c891c87509a9910d3249d70480d20f
Alex Ramos <alex@example.com>
2018-11-14 09:33:27 +0100

    Navigate from homepage to storepage

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,17 +1,36 @@
 import React from "react";
+import { getFunName } from "../helpers";
+import { navigate } from "@reach/router";
 
 class StorePicker extends React.Component {
-  handleClick = () => {
+  inputStore = React.createRef();
+
+  goToStore = event => {
+    event.preventDefault();
+    const myInput = this.inputStore.current;
+    const storeName = myInput.value;
+    navigate(`/store/${storeName}`);
+  };
+
+  /*  
+    handleClick = () => {
     console.log(this);
     alert("Hola, holita vecinitoooooo!!");
   };
+  */
 
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter a Store</h2>
-        <input type="text" required placeholder="Store Name" />
-        <button onClick={this.handleClick} type="submit">
+        <input
+          type="text"
+          required
+          ref={this.inputStore}
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+        />
+        <button type="submit">
           Visit Store{" "}
           <span role="img" aria-labelledby="tenedor">
             🍴
